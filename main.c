18e7c40f0411b3ffbc940cda4289d554758b98a3
Piotr Chmielowiec <chpiotr06@gmail.com>
2022-12-10 15:44:35 +0100

    single route structure

--- main.c
+++ main.c
@@ -7,7 +7,18 @@ typedef struct HTTP_Server {
     int port;
 } HTTP_Server;
 
+typedef struct singleRoute {
+    char* key;
+    char* val;
+    struct singleRoute *prev, *next;
+} singleRoute ;
+
 void initServer(HTTP_Server * server, int port);
+
+singleRoute * initRoute(char* key, char* val);
+singleRoute * addRoute(singleRoute * baseRoute, char* key, char* val);
+singleRoute * getRoute(singleRoute * baseRoute, char* key);
+
 
 int main(){
 
