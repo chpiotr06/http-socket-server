2e9f51a6ff6e28f1878d109090e72eda32697782
Piotr Chmielowiec <chpiotr06@gmail.com>
2022-12-10 15:35:50 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+
+
+
+
+typedef struct HTTP_Server {
+    int socket;
+    int port;
+} HTTP_Server;
+
+void initServer(HTTP_Server * server, int port);
+
+int main(){
+
+}
+
+void initServer(HTTP_Server * server, int port){
+
+}
